e4a0e79292fa595f7731feb5bfed4bb2ea9fa984
waivey <waivey@gmail.com>
2019-09-26 15:37:08 +0100

    to fahrenheit and back funcs added and converter func added

--- src/boilCalc.js
+++ src/boilCalc.js
@@ -12,6 +12,24 @@ function BoilingVerdict(props) {
     return <p>The water would not boil.</p>
 }
 
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 /9;
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature)
+    if (Number.inNan(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
 class TemperatureInput extends React.Component {
     constructor(props) {
         super(props);
